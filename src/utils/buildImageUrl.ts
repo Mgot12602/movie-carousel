74f014426374a3236ed618a4f58a5accb4f3c6c1
Mgot12602 <63016167+Mgot12602@users.noreply.github.com>
2025-07-23 20:17:42 +0200

    chore: updated env sample

--- src/utils/buildImageUrl.ts
+++ src/utils/buildImageUrl.ts
@@ -1,10 +1,13 @@
 import { ImageSizes } from "@/types/movie";
 
-function buildImageUrl(imagePath: string | null, imageSize: ImageSizes) {
+function buildImageUrl(
+  imagePath: string | null,
+  imageSize: ImageSizes
+): string {
   try {
     const baseUrl = process.env.IMAGES_API_BASE_URL;
     if (!baseUrl) {
-      console.error("IMAGES_API_BASE_URL environment variable is not set");
+      throw new Error("IMAGES_API_BASE_URL environment variable is not set");
     }
     const path = imageSize + imagePath;
     return new URL(path, baseUrl).toString();
